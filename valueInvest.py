0b1ce7612b75698d311bdad367413df6b9934bff
Kheng Wei Ang <khengwei118@gmail.com>
2021-05-04 22:41:18 -0700

    constructed advanced stats dataframe

--- valueInvest.py
+++ valueInvest.py
@@ -65,8 +65,60 @@ def portfolio_input():
         print("That's not a number! \n Try again:")
         portfolio_size = input("Enter the value of your portfolio:")
 
-portfolio_input()
-position_size = float(portfolio_size)/len(final_dataframe.index)
-for row in final_dataframe.index:
-    final_dataframe.loc[row, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[row, 'Price'])
-print(final_dataframe)
+###portfolio_input()
+###position_size = float(portfolio_size)/len(final_dataframe.index)
+###for row in final_dataframe.index:
+###    final_dataframe.loc[row, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[row, 'Price'])
+#print(final_dataframe)
+
+symbol = 'AAPL'
+batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,advanced-stats&token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(batch_api_call_url).json()
+print(data['AAPL']['advanced-stats'])
+#Price-to-earnings ratio
+pe_ratio = data[symbol]['quote']['peRatio']
+
+#Price-to-book ratio
+pb_ratio = data[symbol]['advanced-stats']['priceToBook']
+print(pb_ratio)
+
+#Price-to-sales ratio
+ps_ratio = data[symbol]['advanced-stats']['priceToSales']
+print(ps_ratio)
+
+#Enterprise Value divided by Earnings Before Interest, Taxes, Depreciation, and Amortization (EV/EBITDA)
+enterprise_value = data[symbol]['advanced-stats']['enterpriseValue']
+ebitda = data[symbol]['advanced-stats']['EBITDA']
+ev_to_ebitda = enterprise_value / ebitda
+print(ev_to_ebitda)
+
+#Enterprise Value divided by Gross Profit (EV/GP)
+gross_profit = data[symbol]['advanced-stats']['grossProfit']
+ev_to_gross_profit = enterprise_value / gross_profit
+print(ev_to_gross_profit)
+
+# dataframe using robust value
+rv_columns = {
+    'Ticker',
+    'Price',
+    'Number of Shares to Buy',
+    'Price-to-Earnings Ratio',
+    'PE Percentile',
+    'Price-to-Book Ratio',
+    'PB Percentile',
+    'Price-to-Sales Ratio',
+    'PS Percentile',
+    'EV/EBITDA',
+    'EV/EBITDA Percentile',
+    'EV/GP',
+    'EV/GP Percentile',
+    'RV Score'
+}
+
+rv_dataframe = pd.DataFrame(columns = rv_columns)
+print(rv_dataframe)
+
+for symbol_string in symbol_strings:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote,advanced-stats&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url)
+    print(data.status_code)
