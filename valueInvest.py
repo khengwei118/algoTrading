bdd2d4f9ebc649ddb651cf43afceb3015f15363b
Kheng Wei Ang <khengwei118@gmail.com>
2021-05-04 21:00:32 -0700

    added batch call

--- valueInvest.py
+++ valueInvest.py
@@ -6,4 +6,26 @@ from scipy import stats
 import math
 
 stocks = pd.read_csv('sp_500_stocks.csv')
-stocks['Ticker']
+from secrets import IEX_CLOUD_API_TOKEN
+
+symbol = 'aapl'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+#print(type(data))
+
+price = data['latestPrice']
+pe_ratio = data['peRatio']
+print(price, pe_ratio)
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]   
+        
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+    print(symbol_strings[i])
+
+my_columns = ['Ticker', 'Price', 'Price-to-Earnings Ratio', 'Number of Shares to Buy']
