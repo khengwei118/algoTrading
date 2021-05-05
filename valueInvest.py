3352868cd3c88d9c417927ebbec29b35ebebba98
Kheng Wei Ang <khengwei118@gmail.com>
2021-05-04 21:30:32 -0700

    created dataframe

--- valueInvest.py
+++ valueInvest.py
@@ -15,7 +15,7 @@ data = requests.get(api_url).json()
 
 price = data['latestPrice']
 pe_ratio = data['peRatio']
-print(price, pe_ratio)
+#print(price, pe_ratio)
 
 
 def chunks(lst, n):
@@ -26,6 +26,26 @@ symbol_groups = list(chunks(stocks['Ticker'], 100))
 symbol_strings = []
 for i in range(0, len(symbol_groups)):
     symbol_strings.append(','.join(symbol_groups[i]))
-    print(symbol_strings[i])
+#    print(symbol_strings[i])
 
 my_columns = ['Ticker', 'Price', 'Price-to-Earnings Ratio', 'Number of Shares to Buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+for symbol_string in symbol_strings:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    #print(data.status_code)
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
+            pd.Series(
+                [
+                    symbol,
+                    data[symbol]['quote']['latestPrice'],
+                    data[symbol]['quote']['peRatio'],
+                    'N/A'
+                ],
+                index = my_columns
+            ),
+            ignore_index = True
+        )
+#        print(symbol)
+print(final_dataframe)
