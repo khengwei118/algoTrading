1acfdc0f29e180fdf4d65148999327a2cf280f16
Kheng Wei Ang <khengwei118@gmail.com>
2021-05-04 21:52:13 -0700

    sort PE ratios

--- valueInvest.py
+++ valueInvest.py
@@ -8,16 +8,15 @@ import math
 stocks = pd.read_csv('sp_500_stocks.csv')
 from secrets import IEX_CLOUD_API_TOKEN
 
-symbol = 'aapl'
-api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
-data = requests.get(api_url).json()
-#print(type(data))
 
-price = data['latestPrice']
-pe_ratio = data['peRatio']
-#print(price, pe_ratio)
+#symbol = 'aapl'
+#api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+#data = requests.get(api_url).json()
 
+#price = data['latestPrice']
+#pe_ratio = data['peRatio']
 
+# batch call
 def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]   
@@ -26,14 +25,13 @@ symbol_groups = list(chunks(stocks['Ticker'], 100))
 symbol_strings = []
 for i in range(0, len(symbol_groups)):
     symbol_strings.append(','.join(symbol_groups[i]))
-#    print(symbol_strings[i])
 
+# building dataframe
 my_columns = ['Ticker', 'Price', 'Price-to-Earnings Ratio', 'Number of Shares to Buy']
 final_dataframe = pd.DataFrame(columns = my_columns)
 for symbol_string in symbol_strings:
     batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={IEX_CLOUD_API_TOKEN}'
     data = requests.get(batch_api_call_url).json()
-    #print(data.status_code)
     for symbol in symbol_string.split(','):
         final_dataframe = final_dataframe.append(
             pd.Series(
@@ -47,5 +45,11 @@ for symbol_string in symbol_strings:
             ),
             ignore_index = True
         )
-#        print(symbol)
+
+# sorting by lowest PE and removing negative PEs
+final_dataframe.sort_values('Price-to-Earnings Ratio', ascending = True, inplace = True)
+final_dataframe = final_dataframe[final_dataframe['Price-to-Earnings Ratio'] > 0]
+final_dataframe = final_dataframe[:50]
+final_dataframe.reset_index(inplace = True)
+final_dataframe.drop('index', axis = 1, inplace = True)
 print(final_dataframe)
