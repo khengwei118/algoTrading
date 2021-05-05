4edbb3cb6f23025fb09ac4e1c9d938f2ecfc153a
Kheng Wei Ang <khengwei118@gmail.com>
2021-05-05 00:37:09 -0700

    added output to spreadsheet functionality

--- valueInvest.py
+++ valueInvest.py
@@ -179,7 +179,7 @@ metrics = {'Price-to-Earnings Ratio': 'PE Percentile',
 
 for metric in metrics.keys():
     for row in rv_dataframe.index:
-        rv_dataframe.loc[row, metrics[metric]] = score(rv_dataframe[metric], rv_dataframe.loc[row, metric])
+        rv_dataframe.loc[row, metrics[metric]] = score(rv_dataframe[metric], rv_dataframe.loc[row, metric])/100
 #print(rv_dataframe)
 
 # calculating RV score
@@ -204,3 +204,76 @@ position_size = float(portfolio_size) / len(rv_dataframe.index)
 for row in rv_dataframe.index:
     rv_dataframe.loc[row, 'Number of Shares to Buy'] = math.floor(position_size / rv_dataframe.loc[row, 'Price'])
 #print(rv_dataframe)
+
+# excel output
+writer = pd.ExcelWriter('value_strategy.xlsx', engine = 'xlsxwriter')
+rv_dataframe.to_excel(writer, sheet_name = "Value Strategy", index = False)
+#writer.save()
+
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_template = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_template = writer.book.add_format(
+        {
+            'num_format': '$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_template = writer.book.add_format(
+        {
+            'num_format': '0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+float_template = writer.book.add_format(
+        {
+            'num_format': '0.0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+percent_template = writer.book.add_format(
+        {
+            'num_format': '0.0%',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+column_formats = {
+                     'A': ['Ticker', string_template],
+                     'B': ['Price', dollar_template],
+                     'C': ['Number of Shares to Buy', integer_template],
+                     'D': ['Price-to-Earnings Ratio', float_template],
+                     'E': ['PE Percentile', percent_template],
+                     'F': ['Price-to-Book Ratio', float_template],
+                     'G': ['PB Percentile', percent_template],
+                     'H': ['Price-to-Sales Ratio', float_template],
+                     'I': ['PS Percentile', percent_template],
+                     'J': ['EV/EBITDA', float_template],
+                     'K': ['EV/EBITDA Percentile', percent_template],
+                     'L': ['EV/GP', float_template],
+                     'M': ['EV/GP Percentile', percent_template],
+                     'N': ['RV Score', percent_template]
+                  }
+for column in column_formats.keys():
+    writer.sheets['Value Strategy'].set_column(f'{column}:{column}', 25, column_formats[column][1])
+    writer.sheets['Value Strategy'].write(f'{column}1', column_formats[column][0], column_formats[column][1])
+writer.save()
